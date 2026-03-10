59ba61b02b9f7b4ecd97c54b41a5cede655fd071
Brijesh-09 <brijeshkori22@gmail.com>
2026-03-10 23:18:40 +0530

    boilerplate update

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
+
+const authRoutes= require("./routes/auth");
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth" , authRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000")
